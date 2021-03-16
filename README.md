413b80254732a2eb618a3278c1c1eb90635521a7
kyeongwan <kang@kyeongwan.kr>
2021-03-17 01:44:23 +0900

    feat: Initial commit part2 chapter05 🎉

---
+++ README.md
@@ -0,0 +1,49 @@
+# aop-part2-chapter05 - 전자액자
+
+
+
+# 목차
+
+1. 인트로  (완성앱 & 구현 기능 소개)
+2. Android 기기 권한 받아오기
+3. 권한을 통해 사진 가져오기
+4. 사진을 가져와서 목록 구성하기
+5. 액자 화면 구성하기
+6. Activity Lifecycle을 알아보고 완성도 높이기
+7. 아웃트로
+
+
+
+# 결과화면
+
+
+
+![1](./screenshot/1.png)
+
+![2](./screenshot/2.png)
+
+![3](./screenshot/3.png)
+
+# 이 챕터를 통해 배우는 것
+
+- **Layout** 을 그리는 법
+  - 가로 화면으로 그리기
+- **Android Permission** 사용하기
+- **View Animation** 사용하기
+- **Activity Lifecycle** 알아보기
+  - https://developer.android.com/guide/components/activities/activity-lifecycle?hl=ko
+- Content Provider
+  - SAF (Storage Access Framework)
+
+
+
+### 전자액자
+
+저장소 접근 권한을 이용하여 로컬 사진을 로드 할 수 있음.
+
+추가한 사진들을 일정한 간격으로 전환하여 보여줄 수 있음.
+
+
+
+
+
---
+++ app/src/main/java/fastcampus/aop/part2/chapter05_final/PhotoFrameActivity.kt
@@ -0,0 +1,93 @@
+package fastcampus.aop.part2.chapter05_final
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.timer
+
+class PhotoFrameActivity: AppCompatActivity() {
+
+    private val photoList = mutableListOf<Uri>()
+
+    private var currentPosition = 0
+
+    private var timer: Timer? = null
+
+    private val photoImageView: ImageView by lazy {
+        findViewById<ImageView>(R.id.photoImageView)
+    }
+
+    private val backgroundPhotoImageView: ImageView by lazy {
+        findViewById<ImageView>(R.id.backgroundPhotoImageView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photoframe)
+
+        Log.d("PhotoFrame", "onCreate!!!")
+
+        getPhotoUriFromIntent()
+    }
+
+    private fun getPhotoUriFromIntent() {
+        val size = intent.getIntExtra("photoListSize", 0)
+        for (i in 0..size) {
+            intent.getStringExtra("photo$i")?.let {
+                photoList.add(Uri.parse(it))
+            }
+        }
+    }
+
+    private fun startTimer() {
+        timer = timer(period = 5 * 1000) {
+            runOnUiThread {
+
+                Log.d("PhotoFrame", "5초가 지나감 !!")
+
+                val current = currentPosition
+                val next = if (photoList.size <= currentPosition + 1) 0 else currentPosition + 1
+
+                backgroundPhotoImageView.setImageURI(photoList[current])
+
+                photoImageView.alpha = 0f
+                photoImageView.setImageURI(photoList[next])
+                photoImageView.animate()
+                    .alpha(1.0f)
+                    .setDuration(1000)
+                    .start()
+
+                currentPosition = next
+            }
+
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("PhotoFrame", "onStop!!! timer cancel")
+        timer?.cancel()
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("PhotoFrame", "onStart!!! timer start")
+        startTimer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("PhotoFrame", "onDestroy!!! timer cancel")
+        timer?.cancel()
+    }
+
+
+
+}
